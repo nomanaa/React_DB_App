26c56457532a89c4cd735f19c152556bcc0156fe
Abdullah Al Noman <48934180+nomanaa@users.noreply.github.com>
2021-12-27 13:32:32 -0600

    Delete db-route.js

--- server/db-route.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-
-const dbRoutes= require('./db-controller.js')
-
-const router= express.Router()
-
-router.get('/all',dbRoutes.refreeList)
-
-module.exports= router
